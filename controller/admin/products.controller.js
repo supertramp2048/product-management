a73e819c494d7387ddbd189f7992c59fd968fc70
ahay2048 <phamhuy27082004@gmail.com>
2025-07-27 00:13:05 +0700

    them trang product trong admin

--- controller/admin/products.controller.js
+++ controller/admin/products.controller.js
@@ -1,5 +1,8 @@
-module.exports.products=(req,res)=> {
+// [GET] /admin/products
+const Products = require("../../models/product.model")
+module.exports.products= async(req,res)=> {
+    const products = await Products.find({})
     res.render("admin/pages/products/index.pug",{
-       
+       products: products
     });
 } 
---
+++ models/product.model.js
@@ -0,0 +1,20 @@
+
+const mongoose = require('mongoose')
+// mongoose.connect('mongodb://127.0.0.1:27017/product-test-01')
+mongoose.connect("mongodb://127.0.0.1:27017/product-test-01", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log("Kết nối MongoDB thành công"))
+    .catch((err) => {
+        console.error("Kết nối MongoDB thất bại:", err.message);
+        process.exit(1); // Dừng app nếu cần
+    });
+const productSchema = new mongoose.Schema({
+    title: String,
+    thumbnail: String,
+    delete: Boolean
+})
+const Product = mongoose.model('product',productSchema , 'products')
+
+module.exports = Product
